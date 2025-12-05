81b551216af0fee72a06a47cdd7e03db29b955ed
Natasha <127nbonilla@frhsd.com>
2025-12-05 14:04:56 -0500

    hola

---
+++ js/script.js
@@ -0,0 +1,9 @@
+var userbox = document.getElementById('userInput');
+//console.log(userbox);
+
+var email = document.getElementById('emailInput');
+//console.log(emailbox);
+
+
+var city = document.getElementById('cityInput');
+//console.log(city);
